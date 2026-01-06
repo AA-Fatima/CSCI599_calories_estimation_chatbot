d14567245c19c8622816d04de84dcc4bef8a9c73
aa-fatima <fatimaaliahmadst@gmail.com>
2026-01-06 04:09:46 +0200

    test

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://csci599-calories-estimation-chatbot-1.onrender.com/'
+  apiUrl: 'https://csci599-calories-estimation-chatbot-1.onrender.com'
 };
